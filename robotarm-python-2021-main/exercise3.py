94af5eb447c876d6d3203b31412ae8932c641b03
alwinW96 <99060433@mydavinci.nl>
2021-10-08 09:08:32 +0200

    commit exercise3

--- robotarm-python-2021-main/exercise3.py
+++ robotarm-python-2021-main/exercise3.py
@@ -1,29 +1,26 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 2')
+robotArm = RobotArm('exercise 3')
 
 # Jouw python instructies zet je vanaf hier:
 robotArm.speed = 2
 
 robotArm.grab()
-for a in range(9):
-    robotArm.moveRight()
+robotArm.moveRight()
 robotArm.drop()
-for a in range(2):
-    robotArm.moveLeft()
+robotArm.moveLeft()
 robotArm.grab()
-for a in range(2):
-    robotArm.moveRight()
+robotArm.moveRight()
 robotArm.drop()
-for a in range(5):
-    robotArm.moveLeft()
+robotArm.moveLeft()
 robotArm.grab()
-for a in range(5):
-    robotArm.moveRight()
+robotArm.moveRight()
 robotArm.drop()
-
-
-
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
 
 
 # Na jouw code wachten tot het sluiten van de window:
